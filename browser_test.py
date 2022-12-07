feabc36a57666a9d64c3c14c45b84000089fdb45
stdll00 <test@example.com>
2022-12-07 22:46:09 +0900

    fix: add example test

--- browser_test.py
+++ browser_test.py
@@ -1,11 +1,12 @@
 import unittest
 from testing.web import webtest  # gazelle:ignore testing.web
 import urllib3 as _
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class BrowserTest(unittest.TestCase):
     def setUp(self):
-        self.driver = webtest.new_webdriver_session()
+        self.driver: WebDriver = webtest.new_webdriver_session()
 
     def tearDown(self):
         try:
@@ -13,7 +14,10 @@ class BrowserTest(unittest.TestCase):
         finally:
             self.driver = None
 
-    # Your tests here
+    def test_sample_google_title(self):
+        self.driver.get("https://google.com")
+        if self.driver.title != "Google":
+            raise ValueError("title seems invalid")
 
 
 if __name__ == "__main__":
